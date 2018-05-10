e9c634902098b0086be2501dc89dee273adfa4e2
Manuel Weiser <manuel@iMac.fritz.box>
2018-05-10 17:59:30 +0200

    Initial Version

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include <Arduino.h>
+
+int sensorPin = A7;
+int PWM = 7;
+int sensorVal;
+int PWMVal;
+bool fanRunning = false;
+
+void setup() {
+  pinMode(sensorPin, INPUT);
+  pinMode(PWM, OUTPUT);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  Serial.println(sensorVal);
+  delay(1000);
+
+  //read sensor value and set upper limit cap
+  sensorVal = analogRead(sensorPin);
+  // Fan an
+  if (sensorVal > 700) {
+    digitalWrite(PWM, HIGH);
+  }
+  if (sensorVal < 500) {
+    digitalWrite(PWM, LOW);
+  }
+}
